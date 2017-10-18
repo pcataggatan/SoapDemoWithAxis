731763122cd883fd63a69ff69971353243ced37a
Pablo Catggatan <pcataggatan@madisoncollege.edu>
2017-10-17 23:44:29 -0500

    Finisch coding test case.

--- src/test/java/gov/weather/NdfdXMLBindingStubTest.java
+++ src/test/java/gov/weather/NdfdXMLBindingStubTest.java
@@ -3,6 +3,7 @@ package gov.weather;
 import org.junit.Test;
 
 import javax.xml.bind.*;
+import java.io.StringReader;
 
 import static org.junit.Assert.*;
 
@@ -13,19 +14,26 @@ public class NdfdXMLBindingStubTest {
         NdfdXMLBindingStub binding = (NdfdXMLBindingStub)
                 new NdfdXMLLocator().getndfdXMLPort();
 
-        String response = binding.latLonListZipCode("53711");
+        String response = binding.latLonListZipCode("53718");
         //assertEquals("Result did not match expected value", "???", response);
         String latlon = unmarshallResult(response);
+        assertEquals("43.0798,-89.3875", latlon);
     }
 
-    public String unmarshallResult(String response) {
-        JAXBContext jaxbContext = new JAXBContext.newInstance(DwmlType.class);
+
+    public String unmarshallResult(String response) throws Exception {
+
+        JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
 
         try {
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(response));
+            return dwml.getLatLonList();
 
-            Unmarshaller
+        } catch (JAXBException e) {
+            e.printStackTrace();
         }
 
-
+        return null;
     }
 }
